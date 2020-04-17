247c0a7ed9b7f6cd20c925c4ed5ee73f3b4ffc67
Mikhail <35425710+GrefferM@users.noreply.github.com>
2020-04-17 09:33:00 +0300

    Corrects the display of the component error

--- src/App/components/Utils/Paper/index.tsx
+++ src/App/components/Utils/Paper/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 
+import { HeightLayout } from '@/context'
 import classes from './index.module.scss'
-
 // Interface indicates
 // what parameters are in the component
 interface iProps {
@@ -9,8 +9,9 @@ interface iProps {
     supTitle: string
 }
 const Paper: React.FC<iProps> = (props: iProps) => {
+    const heightContext = useContext(HeightLayout)
     return (
-        <div className={classes.root}>
+        <div className={classes.root} style={{ minHeight: heightContext.height }}>
             <img src={require('./paper.png').default} alt="" />
             <div className={classes.title}>{props.title}</div>
             <div className={classes.supTitle}>{props.supTitle}</div>
